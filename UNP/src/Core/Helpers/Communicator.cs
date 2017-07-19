e0610b714fd7581f8a0e7a699492128f6629f960
BvdVijgh <benny@vdvijgh.nl>
2017-07-19 16:19:07 +0200

    Forgot to add
    SVN rev 81

---
+++ UNP/src/Core/Helpers/Communicator.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace UNP.Applications {
+
+    public class Communicator : ExecWrapper {
+
+        private static Logger logger = LogManager.GetLogger("Communicator");
+        private string filepath = null;                                             // path to executable
+
+        public Communicator(string filepath) : base(filepath) { }
+
+        new public void close() {
+
+            // Communicator has a button to quit when it receives a F13 keypress
+            SendKeys.SendWait("{F13}");
+        } 
+    }
+}
